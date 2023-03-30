0414e2726969c295b03915eab9cb420951b4eb93
Maksym_enKO <ingool.ua@gmail.com>
2023-03-30 11:16:27 +0300

    added play pause functionality

---
+++ script.js
@@ -0,0 +1,56 @@
+const image = document.querySelector('img'),
+  title = document.getElementById('title'),
+  artist = document.getElementById('artist'),
+  music = document.querySelector('audio'),
+  currentTimeEl = document.getElementById('current-time'),
+  durationEl = document.getElementById('duration'),
+  progress = document.getElementById('progress'),
+  progressContainer = document.getElementById('progress-container'),
+  prevBtn = document.getElementById('prev'),
+  playBtn = document.getElementById('play'),
+  nextBtn = document.getElementById('next');
+// Music
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+];
+
+// Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
